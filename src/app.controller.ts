08f8637ffbf1e00b27d15ca3fdc0773a7c8d484f
Bibin Lopez <bibinlopez36@gmail.com>
2023-07-14 11:53:56 +0530

    created all methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,31 @@
-import { Controller, Get } from "@nestjs/common"
+import { Controller, Get, Post, Put, Delete } from "@nestjs/common"
 
 @Controller()
 export class AppController {
-  @Get()
+  @Get('')
   getAllIncomeReports() {
     return [];
   }
+
+
+  @Get(':id')
+  getAllIncomeById() {
+    return {};
+  }
+
+  @Post()
+  postIncomeReport() {
+    return { success: true, msg: 'created' }
+  }
+
+  @Put(':id')
+  updateIncomeReport() {
+    return { success: true, msg: 'updated' }
+  }
+
+  @Delete(':id')
+  deleteIncomeReport() {
+    return { success: true, msg: 'deleted' }
+  }
+
 }
