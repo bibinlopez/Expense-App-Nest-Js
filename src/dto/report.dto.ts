0c8afb5735e6358f080e12c36a9dbcbbb84b7edb
Bibin Lopez <bibinlopez36@gmail.com>
2023-07-20 15:59:01 +0530

    Response using Interceptor

--- src/dto/report.dto.ts
+++ src/dto/report.dto.ts
@@ -32,11 +32,19 @@ export class ReportResponseDto {
    id: string;
    source: string;
    amount: number;
+
+   @Exclude()
    created_at: Date;
 
    @Exclude()
    updated_at: Date;
+
    type: ReportType
+
+   @Expose({ name: "createdAt" })
+   transformCreatedAt() {
+      return this.created_at;
+   }
 
    constructor(partial: Partial<ReportResponseDto>) {
       Object.assign(this, partial)
